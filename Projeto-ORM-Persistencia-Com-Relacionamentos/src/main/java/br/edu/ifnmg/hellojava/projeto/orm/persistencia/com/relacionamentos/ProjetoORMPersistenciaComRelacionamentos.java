71ddaa091a50ec1546f3c53fab86391dd7636e7a
claud <claud@DESKTOP-BLA37F1>
2023-11-17 11:40:57 -0300

    Fazendo testes

--- Projeto-ORM-Persistencia-Com-Relacionamentos/src/main/java/br/edu/ifnmg/hellojava/projeto/orm/persistencia/com/relacionamentos/ProjetoORMPersistenciaComRelacionamentos.java
+++ Projeto-ORM-Persistencia-Com-Relacionamentos/src/main/java/br/edu/ifnmg/hellojava/projeto/orm/persistencia/com/relacionamentos/ProjetoORMPersistenciaComRelacionamentos.java
@@ -1,17 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package br.edu.ifnmg.hellojava.projetoorm.persistencia.com.relacionamentos;
 
-import test.TestCrud;
+package br.edu.ifnmg.hellojava.projeto.orm.persistencia.com.relacionamentos;
 
 /**
  *
  * @author Claudio Alcantara &lt;claudio.alcantara at ifnmg.edi.br&gt;
  */
-public class ProjetoOrmPersistenciaComRelacionamentos {
+public class ProjetoORMPersistenciaComRelacionamentos {
+
     public static void main(String[] args) {
 //        TestCrud.testeA();
 //        TestCrud.testeB();
