a62e85d3a20b2a82d76136ebc4727816979e1bf8
claud <claud@DESKTOP-BLA37F1>
2023-11-17 10:50:45 -0300

    Inicio do projeto.

---
+++ Projeto-ORM-Persistencia-Com-Relacionamentos/src/main/java/br/edu/ifnmg/hellojava/projeto/orm/persistencia/com/relacionamentos/ProjetoORMPersistenciaComRelacionamentos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifnmg.hellojava.projeto.orm.persistencia.com.relacionamentos;
+
+/**
+ *
+ * @author Claudio Alcantara &lt;claudio.alcantara at ifnmg.edi.br&gt;
+ */
+public class ProjetoORMPersistenciaComRelacionamentos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
