eb69ddb502f5c9fa84015152b8dce0b47fe2ba7e
Anne Karolinne Dias <58616039+annekarolinneds@users.noreply.github.com>
2023-11-17 11:18:49 -0300

    Add Reader

---
+++ Projeto-ORM-Persistencia-Com-Relacionamentos/src/main/java/reader/Reader.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reader;
+
+import user.User;
+
+/**
+ *
+ * @author annekarolinneds;
+ */
+public class Reader extends User{
+
+    public Reader() {
+    }
+    
+    public Reader(User user){
+        try{
+            setName(user.getName());
+            setEmail(user.getEmail());
+            setBirthDate(user.getBirthDate());
+            setCredential(user.getCredentical());
+            setRole(user.getRole());
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
+    }
+}
---
+++ Projeto-ORM-Persistencia-Com-Relacionamentos/src/main/java/reader/ReaderDao.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reader;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import repository.Dao;
+
+/**
+ *
+ * @author annekarolinneds;
+ */
+public class ReaderDao extends Dao<Reader>{
+    public final String TABLE = "reader";
+
+    @Override
+    public String getSaveStatement() {
+        return "INSERT INTO " + TABLE + "(id) VALUES (?)";
+    }
+
+    @Override
+    public String getUpdateStatement() {
+        return "INSERT INTO " + TABLE + "(id) VALUES (?)";
+    }
+
+    @Override
+    public String getFindByIdStatement() {
+        return "SELECT id FROM " + TABLE + " WHERE id = ?";
+    }
+
+    @Override
+    public String getFindAllStatement() {
+        return "SELECT id FROM " + TABLE;
+    }
+
+    @Override
+    public String getDeleteStatement() {
+        return "DELETE FROM " + TABLE + " WHERE id = ?";
+    }
+
+    @Override
+    public void coposeSaveOrUpdateStatement(PreparedStatement pstmt, Reader e) {
+        try {
+            pstmt.setLong(1, e.getId());
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ReaderDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public Reader extractObject(ResultSet rs) {
+        Reader reader = new Reader();
+        try{
+            reader.setId(rs.getLong("id"));
+        }catch(Exception ex){
+                System.out.println("Ex: " + ex);      
+        }
+        return reader;
+    }
+
+    @Override
+    public List<Reader> extractObjects(ResultSet rs) {
+        List<Reader> readers = new ArrayList<>();
+        try{
+            while(rs.next()){
+                Reader reader = new Reader();
+                reader.setId(rs.getLong("id"));
+                readers.add(reader);
+            }
+        }catch(Exception ex){
+                System.out.println("Ex: " + ex);      
+        }   
+        return readers;
+    }
+
+    public ReaderDao() {
+    }
+
+    public String getTABLE() {
+        return TABLE;
+    }
+
+    public static String getDB() {
+        return DB;
+    }
+    
+    
+}
