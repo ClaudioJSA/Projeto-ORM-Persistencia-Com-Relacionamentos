7243858bcf4ea501e826a88682becd14e5689d83
claud <claud@DESKTOP-BLA37F1>
2023-12-05 08:37:29 -0300

    Implementando CommentDao

--- Projeto-ORM-Persistencia-Com-Relacionamentos/src/main/java/comment/CommentDao.java
+++ Projeto-ORM-Persistencia-Com-Relacionamentos/src/main/java/comment/CommentDao.java
@@ -7,7 +7,12 @@ package comment;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import reader.Reader;
 import repository.Dao;
 
 /**
@@ -15,45 +20,74 @@ import repository.Dao;
  * @author Claudio Alcantara &lt;claudio.alcantara at ifnmg.edi.br&gt;
  */
 public class CommentDao extends Dao<Comment>{
-
+    public final String TABLE = "comment";
+    
     @Override
     public String getSaveStatement() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "INSERT INTO " + TABLE + "(comment, idreader, id) VALUES (?, ?, ?)";
     }
-
+    
     @Override
     public String getUpdateStatement() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "UPDATE " + TABLE + " SET comment = ?, idreader = ? WHERE id = ?";
     }
 
     @Override
     public String getFindByIdStatement() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "SELECT comment, idreader FROM " + TABLE + " WHERE id = ?";
     }
 
     @Override
     public String getFindAllStatement() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "SELECT comment, idreader FROM " + TABLE;
     }
 
     @Override
     public String getDeleteStatement() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "DELETE FROM " + TABLE + " WHERE id = ?";
     }
 
     @Override
     public void coposeSaveOrUpdateStatement(PreparedStatement pstmt, Comment e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            pstmt.setString(1, e.getComment());
+            pstmt.setLong(2, e.getReader().getId());
+            if (e.getId() != null) {
+                pstmt.setLong(3, e.getId());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CommentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public Comment extractObject(ResultSet rs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Comment comment = new Comment();
+        try{
+            comment.setReader(new Reader());
+            comment.setComment(rs.getString("comment"));
+            comment.getReader().setId(rs.getLong("idreader"));
+        }catch(Exception ex){
+                System.out.println("Ex: " + ex);      
+        }
+        return comment;
     }
 
     @Override
     public List<Comment> extractObjects(ResultSet rs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Comment> comments = new ArrayList<>();
+        try{
+            while(rs.next()){
+                Comment comment = new Comment();
+                comment.setReader(new Reader());
+                comment.setComment(rs.getString("comment"));
+                comment.getReader().setId(rs.getLong("idreader"));
+                comments.add(comment);
+            }
+        }catch(Exception ex){
+                System.out.println("Ex: " + ex);      
+        }   
+        return comments;
     }
     
 }
